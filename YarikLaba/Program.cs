04443a4d1135a68b84550a0f9ac946bb0113d3bc
Denis Denis <d_glinskiy@inbox.ru>
2021-12-03 22:51:29 +0300

    Добавьте файлы проекта.

---
+++ YarikLaba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YarikLaba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
